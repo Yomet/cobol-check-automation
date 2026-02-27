0b9aa3bbb014a181ce6b27ea2c351bf0ace3a072
Yomet <yomet10@gmail.com>
2026-02-27 15:32:16 +0100

    Refactor zowe_operations.sh to use variables and checks
    Removed hardcoded values for z/OSMF connection and added checks for directory existence.

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -7,24 +7,20 @@ LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 
 #Debugging the zowe CLI installation
 echo "======================================================="
-echo "Testing the connection to z/OSMF using hardcoded values"
-echo "======================================================="
-zowe zosmf check status --host "204.90.115.200" --port "10443" --user "Z79951" --pass "SAI29IZI" --reject-unauthorized false
-
-echo "======================================================="
 echo "Testing the connection to z/OSMF using User & Pass variables"
 echo "======================================================="
 zowe zosmf check status --host "204.90.115.200" --port "10443" --user "$ZOWE_USERNAME" --pass "$ZOWE_PASSWORD" --reject-unauthorized false
 
-#echo "======================================================="
-#echo "Testing the connection to z/OSMF using profile"
-#echo "======================================================="
-#zowe zosmf check status
+echo "======================================================="
+echo "Trying to run the zowe commands with hardcoded values"
+echo "======================================================="
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" --host "204.90.115.200" --port "10443" --user "$ZOWE_USERNAME" --pass "$ZOWE_PASSWORD" --reject-unauthorized false &>/dev/null; then
+  echo "Directory does not exist. Creating it..."
+#  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+else
+  echo "Directory already exists."
+fi
 
-echo "======================================================="
-echo "Trying to initialize a profile so I can fill it"
-echo "======================================================="
-zowe config init
 
 #echo "Running the first zowe command"
 #if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
