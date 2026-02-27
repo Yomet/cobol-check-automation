49de23933fad3b6f8afba5c7306412334d2adf1f
Yomet <yomet10@gmail.com>
2026-02-27 15:44:43 +0100

    Refactor zowe_operations.sh for directory checks and uploads

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -4,8 +4,7 @@
 # Convert username to lowercase
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 PARAM_STRING="--host \"204.90.115.200\" --port \"10443\" --user \"$ZOWE_USERNAME\" --pass \"$ZOWE_PASSWORD\" --reject-unauthorized false"
-# Check if directory exists, create if it doesn't
-
+DEBUGGING_INFO_TO_BE_DELETED='
 #Debugging the zowe CLI installation
 echo "======================================================="
 echo $PARAM_STRING
@@ -32,17 +31,18 @@ if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $PARAM_ST
 else
   echo "Directory already exists."
 fi
+'
 
-
-#echo "Running the first zowe command"
-#if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
-#  echo "Directory does not exist. Creating it..."
-#  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
-#else
-#  echo "Directory already exists."
-#fi
-## Upload files
-#zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar"
-## Verify upload
-#echo "Verifying upload:"
-#zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+# Check if directory exists, create if it doesn't
+echo "Running the first zowe command"
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $PARAM_STRING &>/dev/null; then
+  echo "Directory does not exist. Creating it..."
+  zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck $PARAM_STRING 
+else
+  echo "Directory already exists."
+fi
+# Upload files
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar" $PARAM_STRING 
+# Verify upload
+echo "Verifying upload:"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" $PARAM_STRING 
